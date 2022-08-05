af381221ea042d78c8ea6d1ed0b5781c84ff8fa2
Emerson Ingram <44170688+eingram00@users.noreply.github.com>
2022-08-05 14:06:29 -0400

    Update main.py
    Added resources file to end of script.

--- main.py
+++ main.py
@@ -101,3 +101,37 @@ def machine_start():
 
 # Go Time
 machine_start()
+
+
+#### Resources place in a different file
+# MENU = {
+#     "espresso": {
+#         "ingredients": {
+#             "water": 50,
+#             "coffee": 18,
+#         },
+#         "cost": 1.5,
+#     },
+#     "latte": {
+#         "ingredients": {
+#             "water": 200,
+#             "milk": 150,
+#             "coffee": 24,
+#         },
+#         "cost": 2.5,
+#     },
+#     "cappuccino": {
+#         "ingredients": {
+#             "water": 250,
+#             "milk": 100,
+#             "coffee": 24,
+#         },
+#         "cost": 3.0,
+#     }
+# }
+
+# resources = {
+#     "water": 300,
+#     "milk": 200,
+#     "coffee": 100,
+# }
